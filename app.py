7d729bcce2822d9dae4180dc67f0b1228c2670de
bjacobsen <32605341+Bizzebgee@users.noreply.github.com>
2020-01-19 13:20:02 -0500

    added config.py and filesystem storage

--- app.py
+++ app.py
@@ -2,24 +2,29 @@
 from flask import Flask, request, Response
 import jsonpickle
 import numpy as np
+import hashlib
 import re
 import cv2
 import subprocess
+from datetime import date
 app = Flask(__name__)
+app.config.from_object('config')
 
-
-@app.route('/api/test', methods=['POST'])
+@app.route('/api/upload', methods=['POST'])
 def test():
     r = request
     # convert string of image data to uint8
     nparr = np.fromstring(r.data, np.uint8)
     # decode image
     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-    img_path = r'C:\Projects\FALdetector-master\examples\modified.jpg'
+    # get by millisecond instead
+    #dt = datetime.now()
+    #img_name = hashlib.sha224(dt + ).hexdigest()
+    img_path = app.config["BASE_IMAGE_PATH"] + 'test.jpg'
+    cv2.imwrite(img_path, img)
     call_str = r'python C:\Projects\FALdetector-master\global_classifier.py --model_path C:\Projects\FALdetector-master\weights\global.pth --input_path ' + img_path
     print(call_str.split())
-#subprocess.Popen(['cd', 'C:/Projects/FALdetector-master'], shell=True)
+    return Response(response={'fake_chance': ""}, status=200, mimetype="application/json")
     proc = subprocess.Popen(args=call_str.split(), stdout=subprocess.PIPE, cwd=r'C:\Projects\FALdetector-master')
     proc.wait()
     #if no errors
--- test_clients/client.py
+++ test_clients/client.py
@@ -1,8 +1,9 @@
+from requests.auth import HTTPBasicAuth
 import requests
 import json
 import subprocess
 addr = 'http://localhost:5000'
-test_url = addr + '/api/test'
+test_url = addr + '/api/upload'
 
 # prepare headers for http request
 content_type = 'image/jpeg'
@@ -10,6 +11,6 @@ headers = {'content-type': content_type}
 img_path = 'C:/Projects/FALdetector-master/examples/modified.jpg'
 img = open(img_path, 'rb').read()
 
-response = requests.post(test_url, data=img, headers=headers)
+response = requests.post(test_url, data=img, auth=HTTPBasicAuth('Ben', 'password'), headers=headers)
 # decode response
 print(json.loads(response.text))
