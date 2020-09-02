2dc608e3af7358face09f1905ab2aef2117909c3
BenJacobsen <bbjacobs@uwaterloo.ca>
2020-09-02 15:41:03 -0400

    add images to db and rabbitmq basic

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 # app.py
 from flask import Flask, request, Response, jsonify
 from functools import wraps
+from enum import Enum
 from pymongo import MongoClient
 from exceptions import Unauthorized, BadRequest
+import pika
 from auth import Credentials
 from datetime import datetime
 import jsonpickle
@@ -17,6 +19,11 @@ app = Flask(__name__)
 app.config.from_object('config')
 client = MongoClient(app.config["MONGO_URI"])
 db = client["admin"]
+
+class ImageStatus(Enum):
+    RUNNING = 1
+    CANCELED = 2
+    COMPLETE = 3
 
 @app.errorhandler(Unauthorized)
 def handle_invalid_usage(error):
@@ -49,24 +56,34 @@ def construct_call_str(img_path):
 @authenticate
 def image_upload():
     r = request
-    raise BadRequest('Authenticated!')
     # convert string of image data to uint8
     nparr = np.fromstring(r.data, np.uint8)
     # decode image
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    #img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     # get by millisecond instead
     now = datetime.now()
-    hash_str = now.strftime("%m/%d/%Y, %H:%M:%S") + r.authorization.username
-    img_name = hashlib.sha224(hash_str.encode('utf-8')).hexdigest() + ".jpg"
-    img_path = app.config["BASE_IMAGE_PATH"] + r.authorization.username
-    cv2.imwrite(img_path, img)
+    
+    hash_str = hashlib.sha224((now.strftime("%m/%d/%Y, %H:%M:%S") + r.authorization.username).encode('utf-8')).hexdigest() #hash by ms as two requests could be sent in a sec
+    #check against mongo to see if hash exists, recreate hash if it does. if not create entry "not done" if its not done
+    while not db.images.find_one({ "id":  hash_str }) is None:
+        hash_str = hashlib.sha224((now.strftime("%m/%d/%Y, %H:%M:%S") + r.authorization.username).encode('utf-8')).hexdigest()
+    db.images.insert_one({ "id": hash_str, "username": r.authorization.username, "timeCreated": now.strftime("%m/%d/%Y, %H:%M:%S"), "status": ImageStatus.RUNNING.value })
+    #img_path = app.config["BASE_IMAGE_PATH"] + r.authorization.username
+    #cv2.imwrite(img_path, img)
     #proc = subprocess.Popen(args=construct_call_str(img_path).split(), stdout=subprocess.PIPE, cwd=app.config["IMG_PROCESS_PATH"])
     #proc.wait()
     #if no errors
     #stdout = proc.stdout.read().decode('utf-8')
     #fake_chance = re.findall(r'[0-9]+', stdout)
-    response = {'fake_chance': 99}
-    response_pickled = jsonpickle.encode(response)
+    #response = {'fake_chance': 99}
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
+    channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body=hash_str)
+    connection.close()
+
+    response_pickled = jsonpickle.encode({"imageCode": hash_str})
     return Response(response=response_pickled, status=200, mimetype="application/json")
 
 @app.route('/users/create', methods=['POST'])
